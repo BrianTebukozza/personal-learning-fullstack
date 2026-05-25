1e7720baeb2e96f06c0fa0c6ec7eabe8dd78596b
brian tebukozza <brintebrn@gmail.com>
2026-05-25 13:03:38 +0300

    JS concepts refresher

---
+++ class.js
@@ -0,0 +1,218 @@
+// let firstName = "Namirembe";
+// let lastName = "Redemptors";
+// console.log(firstName);
+// console.log(lastName);
+
+
+// let age = 11;
+// console.log(age);
+
+// age = 35;
+// console.log(age);
+
+// const pi = 3.14;
+// console.log(pi);
+
+
+
+
+// console.log(23 + 97);
+
+// console.log((4 + 6 + 9) / 77);
+
+// let a = 10;
+// a = 23;
+// console.log(a);
+
+// let b = 7 * a;
+// console.log(b);
+
+// const max = 57;
+
+// const actual = max - 13;
+
+// const percentage = actual / max;
+
+// console.log(percentage);
+
+
+/*let num = "Brian";
+alert(num);*/
+
+//alert(1 / 0);
+
+//alert(`hello my age is ${pi}`);
+
+/*let length = firstName.length;
+console.log(length);
+
+let char = firstName.charAt(7);
+let code = firstName.charCodeAt(8);
+console.log(code);
+
+console.log(char); */
+
+
+
+// let count = lastName.length;
+// console.log(count);
+
+//alert('Gleen' = 'Glow');
+
+
+// operands of different types are converted to numbers by the equality operator ==
+// A strict equality operator === checks the equality without type conversion.
+
+
+
+/*let year = prompt('in which year was i born?');
+
+if (year == 1991) {
+    alert('you are right');
+}
+else if (year > 1992) {
+    alert('you dont know me');
+} else {
+    alert('sm not a fossil')
+};*/
+
+/*let accessAllowed;
+let number = prompt('How old are you?', '');
+if (number > 18) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
+}
+
+alert(accessAllowed);*/
+
+
+/*let openGate = (open > 12) ? true : false;
+
+alert('all the best');*/
+
+
+/*let time = 9;
+let hasAccess = false;
+
+if (time >= 9) {
+    alert('you can come in');
+    if (hasAccess) {
+        alert('park your car in the basement')
+    }
+    else {
+        alert('park your car outside the gate');
+    }
+} else {
+    alert('please wait for time to reach')
+};*/
+
+
+/* let id = 23;
+ let roleManager = true;
+
+ if (id === 23) {
+     alert('open this door');
+     if (roleManager) {
+         alert('welcome manager')
+     } else {
+         ;
+         alert('ask technical team');
+     }
+ } else {
+     alert('please try next door');
+};*/
+
+// const myText = document.getElementById("myText");
+// const mySubmit = document.getElementById("mySubmit");
+// const resultElement = document.getElementById("resultElement");
+
+// let currency;
+
+// mySubmit.onclick = function () {
+
+//     currency = myText.value;
+//     currency = Number(currency);
+//     if (currency >= 18) {
+//         resultElement.textContent = `you can buy this item`
+//     }
+//     else if (currency == 16) {
+//         resultElement.textContent = `item has been discounted`
+//     }
+//     else if (currency <= 10) {
+//         resultElement.textContent = `check out other stalls`
+//     }
+//     else if (currency >= 13) {
+//         resultElement.textContent = `top up 5 shs`
+//     }
+//     else {
+//         resultElement.textContent = `top up to buy this item`
+//     };
+
+// };
+
+
+// const car = "Audi";
+// const speed = 180;
+// const highestSpeed = 200;
+
+// const score = (`My ${car} runs at a speed of ${highestSpeed} \nbut last race i only scored ${speed}. \nThat means i remained with ${highestSpeed - speed} to \nreach my full potential!`);
+
+// alert(score);
+
+
+// const bigBoy = 'He\ teases all his class mates';
+// alert(bigBoy);
+
+const big = 'big head';
+console.log(big);
+
+const head = big;
+console.log(head);
+
+console.log(`i have a\n${big} and my wife has a bigger ${head}`);
+
+const structure = `elephants, has a big ${big}`;
+
+console.log(structure);
+
+const greet = 'muli mutya ba ssebo ne\ ba nnyabo!';
+console.log(greet);
+
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
+
+
+const myBrand = 'brinte';
+const year = 2011;
+console.log(myBrand + year);
+
+
+const brandName = "brinte";
+const contact = Number(brandName);
+console.log(typeof contact);
+
+
+const telephone = 12345;
+const call = String(telephone);
+console.log(typeof call);
+
+
+let telephone2 = "avscdefg";
+let length = telephone2.length;
+console.log(length);
+
+let char = telephone2.charAt(4);
+console.log(char);
+
+let answer = telephone2.charCodeAt(0);
+console.log(answer);
+
+let point = telephone2.codePointAt(5);
+console.log(point);
+
+let names = telephone2.at(3);
+console.log(names);
+
+let brian = telephone2[0];
+console.log(brian);
---
+++ learnJs.js
@@ -0,0 +1,38 @@
+//concatenation
+
+/*let father = "Brian";
+let mother = "Carol";
+let baby = "Brinte";
+//let family1, family2, family3;
+
+//method 1
+//family1 = father + " " + mother + " " + baby;
+
+
+//method 2
+//family2 = father.concat(" ", mother, " ",baby, ".");
+
+//method 3
+//family3 = `${father} ${mother} ${baby}.`;
+
+
+
+//console.log(family1, family2, family3);
+
+
+console.log("name" + " " + "baby");
+console.log(`the family name is ${father} ${mother} ${baby}`);
+console.log(father.concat(" "+"is" + " " +"the" + " "+ "father"));
+
+*/
+
+//properties
+let desc = " space ";
+console.log(desc.length);
+
+//methods
+console.log(desc);
+console.log(desc.trim())
+
+
+
